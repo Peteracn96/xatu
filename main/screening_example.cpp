7dbbc9536d65acd6d240f09feb96fea04769dcd5
Peteracn96 <pacn96@gmail.com>
2024-10-04 17:48:23 +0200

    Example of script using screening.

---
+++ main/screening_example.cpp
@@ -0,0 +1,28 @@
+#include <armadillo>
+#include <xatu.hpp>
+
+
+int main(){
+
+    arma::rowvec q = {1.2, 0.3, 0};
+
+    int vbands = 34;
+    int cbands = 16;
+    int Ncells = 15;
+    double Gcutoff = 25;
+
+    auto model_config = xatu::SystemConfiguration("../examples/material_models/wannier_models/MoS2_spin_wannier_07032024.model");
+
+    auto exciton_config = xatu::ExcitonConfiguration("../examples/excitonconfig/MoS2_test.txt");
+
+    auto screening_config = xatu::ScreeningConfiguration("../examples/screeningconfig/MoS2_TB_screening.txt");
+
+    auto mos2_exciton = xatu::ExcitonTB(model_config, exciton_config);
+
+    //mos2_exciton.initializeScreeningAttributes(screening_config);
+
+    mos2_exciton.setGcutoff(Gcutoff);
+    
+
+
+}
