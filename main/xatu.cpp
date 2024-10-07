a18df3d1a41940833011fa3e4ab7830c13a1d48b
Peteracn96 <pacn96@gmail.com>
2024-10-07 14:42:21 +0200

    🚸

--- main/xatu.cpp
+++ main/xatu.cpp
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]){
     TCLAP::ValuesConstraint<std::string> allowedMethods(methods);
     TCLAP::ValueArg<std::string> methodArg("m", "method", "Method to solve the Bethe-Salpeter equation.", false, "diag", &allowedMethods, cmd);
     TCLAP::ValueArg<std::string> bandsArg("b", "bands", "Computes the bands of the system on the specified kpoints.", false, "dummy.txt", "Filename", cmd);
-    TCLAP::ValueArg<std::string> screeningArg("z", "screeningfile", "Provides input parameters for computing the screening.", false, "model.screening", "Filename", cmd);
+    TCLAP::ValueArg<std::string> screeningArg("z", "screening", "Provides input parameters for computing the screening.", false, "model.screening", "Filename", cmd);
     TCLAP::UnlabeledValueArg<std::string> systemArg("systemfile", "System file", true, "system.txt", "filename", cmd);
     TCLAP::UnlabeledValueArg<std::string> excitonArg("excitonfile", "Exciton file", false, "exciton.txt", "filename", cmd);
     cmd.parse(argc, argv);
