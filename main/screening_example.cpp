e4c0c486f68298a34c61c1e586457a1d52ba9f2c
Peteracn96 <pacn96@gmail.com>
2024-10-24 10:13:09 -0300

    Work on example

--- main/screening_example.cpp
+++ main/screening_example.cpp
@@ -119,7 +119,9 @@ int main(){
     //     std::cout << "Vector dif. nummer " << i << " was found " << CountRdifs(i) << " times\n";
     // }
 
-    std::set<int> indexes_set = std::set<int>( index_array, index_array + nRdif );
+    std::set<int> indexes_set = std::set<int>(index_array, index_array + nRdif);
+
+    int n_non_equivalent_vectors = indexes_set.size();
 
     std::cout << "The indices are:" <<  std::endl;
 
@@ -128,7 +130,7 @@ int main(){
         std::cout << index << ", ";
     }
 
-    std::cout << "\nTotal number of non equivalent vectors is = " << indexes_set.size() << " vectors" <<  std::endl;
+    std::cout << "\nTotal number of non equivalent vectors is = " << n_non_equivalent_vectors << " vectors" <<  std::endl;
 
     std::cout << "Number of k points in BZ mesh = " << mos2_exciton.system->kpoints.n_rows << std::endl;
 
@@ -136,11 +138,89 @@ int main(){
 
     int NAtoms = mos2_exciton.system->natoms;
 
-    arma::mat T(nRvectors*NAtoms,nRvectors*NAtoms,arma::fill::zeros);
+    int n_rows = nRvectors*NAtoms;
 
-    for (int const& index : indexes_set){
-        //finish this
+    arma::mat T(n_rows,n_rows,arma::fill::zeros);
+
+    // Generates combinations (all of them and non equivalent ones) to compute T
+
+    int n_all_combinations = pow(nRvectors*NAtoms,2);
+    arma::imat all_combinations(n_all_combinations,4,arma::fill::zeros);
+    int i_aux = 0;
+    for (int R_i = 0; R_i < nRvectors; R_i++){
+        
+        for (int R_j = 0; R_j < nRvectors; R_j++){
+            
+            for (int t_i = 0; t_i < NAtoms; ++t_i){
+                
+                for (int t_j = 0; t_j < NAtoms; ++t_j){
+                    all_combinations(i_aux,0) = R_i;
+                    all_combinations(i_aux,1) = t_i;
+                    all_combinations(i_aux,2) = R_j;
+                    all_combinations(i_aux,3) = t_j;
+                    ++i_aux;
+                }
+            }
+        }
     }
+
+    int n_non_equivalent_combinations = n_non_equivalent_vectors*NAtoms*NAtoms;
+
+    arma::imat non_equivalent_combinations(n_non_equivalent_combinations,3,arma::fill::zeros);
+
+    arma::vec T_aux(n_non_equivalent_combinations,arma::fill::zeros);
+    
+    i_aux = 0;
+    for (int const& index : indexes_set){
+        for (int t_i = 0; t_i < NAtoms; ++t_i){
+            for (int t_j = 0; t_j < NAtoms; ++t_j){
+                non_equivalent_combinations(i_aux,0) = index;
+                non_equivalent_combinations(i_aux,1) = t_i;
+                non_equivalent_combinations(i_aux,2) = t_j;
+                ++i_aux;
+            }
+        }
+    }
+
+    // Computes non equivalent matrix elements of T
+
+    for (int i = 0; i < n_non_equivalent_combinations; i++)
+    {
+        int R_dif_index = non_equivalent_combinations(i,0);
+        int t_i_index = non_equivalent_combinations(i,1);
+        int t_j_index = non_equivalent_combinations(i,2);
+
+        arma::rowvec R_dif = Rdifferences.row(R_dif_index);
+        arma::rowvec R_origin(3,arma::fill::zeros);
+
+        T_aux(i) = mos2_exciton.computesinglePolarizability(R_dif, R_origin, t_i_index, t_j_index);
+        std::cout << "i = " << i << ", R_dif = (" << R_dif(0) << "," << R_dif(1) << "), t_i = " << t_i_index << ", t_j = " << t_j_index << std::endl;
+    }
+
+    // Prints the elements
+    for (int i = 0; i < n_non_equivalent_combinations; i++)
+    {
+        std::cout << "T(" << i << ") = " << T_aux(i) << std::endl;
+    }
+    
+    // Builds the big T matrix
+
+    for (int i = 0; i < n_non_equivalent_combinations; i++)
+    {
+        int R_dif_index = non_equivalent_combinations(i,0);
+
+        int R_i = Rdifferences.row(R_dif_index)(3);
+        int R_j = Rdifferences.row(R_dif_index)(4);
+
+        int t_i_index = non_equivalent_combinations(i,1);
+        int t_j_index = non_equivalent_combinations(i,2);
+
+        T(R_i + t_i_index, R_j + t_j_index) = T_aux(i);
+    }
+    
+    // Prints the T matrix
+
+    T.print("T:");
 
     return 0;
 }
