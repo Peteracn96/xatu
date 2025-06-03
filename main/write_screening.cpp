81d43d004c7e0da9127fd168b28d70eb4e8ca571
Peteracn96 <pacn96@gmail.com>
2025-06-03 18:33:57 +0200

    Work with Wannier model

--- main/write_screening.cpp
+++ main/write_screening.cpp
@@ -24,54 +24,58 @@ int main(){
 
     //auto start = high_resolution_clock::now();
 
-    xatu::SystemConfiguration model_config("../examples/material_models/MoS2.model");
+    xatu::SystemConfiguration model_config("../examples/material_models/wannier_models/MoS2_spin_wannier_07032024_tb.model");
 
     xatu::ExcitonConfiguration exciton_config("../examples/excitonconfig/MoS2_test.txt");
 
-    xatu::ScreeningConfiguration screening_config("../examples/screeningconfig/MoS2_TB_screening.txt");
+    xatu::ScreeningConfiguration screening_config("../examples/screeningconfig/MoS2_wannier_screening.txt");
 
-    xatu::ExcitonTB mos2_exciton(model_config, exciton_config,screening_config);
+    xatu::ExcitonTB hBN_exciton(model_config, exciton_config,screening_config);
 
-    mos2_exciton.brillouinZoneMesh(mos2_exciton.ncell);
-    mos2_exciton.initializeHamiltonian();
+    hBN_exciton.brillouinZoneMesh(hBN_exciton.ncell);
+    hBN_exciton.initializeHamiltonian();
 
-    int NGs = mos2_exciton.nReciprocalVectors;
+    hBN_exciton.writeBZtofile();
+
+    int NGs = hBN_exciton.nReciprocalVectors;
 
     std::cout << "Number of reciprocal vectors used in the calculation: " << NGs << std::endl;
 
-    const int array_size = 11;
-    int nGs_array[array_size] = {1,8,9,13,19,30,37,39,43,55,61};
+    // const int array_size = 11;
+    // int nGs_array[array_size] = {1,8,9,13,19,30,37,39,43,55,61};
 
-    for (int gs = 0; gs < array_size; ++gs) {
+    // for (int gs = 0; gs < array_size; ++gs) {
 
-	    int nGs = nGs_array[gs];
+	//     int nGs = nGs_array[gs];
 
-	    mos2_exciton.setReciprocalVectors(nGs);
+	//     hBN_exciton.setReciprocalVectors(nGs);
 
-        // Testing the code for RPA calculation
-        mos2_exciton.compute_2D_RPAInvDielectricMatrix("MoS2_TB_q_points_test.dat");
+    //     // Testing the code for RPA calculation
+    //     hBN_exciton.compute_2D_RPAInvDielectricMatrix("MoS2_TB_q_points_test.dat");
 
-        mos2_exciton.writeRPAPolarizabilityMatrix("MoS2_RPA_polarizability_" + std::to_string(nGs) + ".dat");
+    //     hBN_exciton.writeRPAPolarizabilityMatrix("MoS2_RPA_polarizability_" + std::to_string(nGs) + ".dat");
 
-        mos2_exciton.writeRPAInverseDielectricMatrix("MoS2_TB_RPA_screening_" + std::to_string(nGs) + ".dat");
-    }
+    //     hBN_exciton.writeRPAInverseDielectricMatrix("MoS2_TB_RPA_screening_" + std::to_string(nGs) + ".dat");
+    // }
 
-    //mos2_exciton.compute_2D_DielectricMatrix_Opt("MoS2_TB_q_points_test.dat"); not optimal at the end
+    //hBN_exciton.compute_2D_DielectricMatrix_Opt("MoS2_TB_q_points_test.dat"); not optimal at the end
 
-    //mos2_exciton.compute_2D_DielectricMatrix("MoS2_TB_q_points_test.dat");
+    hBN_exciton.compute_2D_DielectricMatrix("qy_points_MoS2_spin_wannier_semicore.dat");
 
-    //mos2_exciton.invertDielectricMatrix();
+    //hBN_exciton.invertDielectricMatrix();
 
-    //mos2_exciton.writePolarizabilityMatrix("MoS2_polarizability.dat");
+    //hBN_exciton.writePolarizabilityMatrix("MoS2_polarizability.dat");
 
-    //mos2_exciton.writeInverseDielectricMatrix("MoS2_TB_screening.dat");
+    //hBN_exciton.writeInverseDielectricMatrix("MoS2_TB_screening.dat");
+
+    hBN_exciton.writeDielectricMatrix("MoS2_spin_wannier_semicore_qy.dat");
 
     // Testing the code for RPA calculation
-    // mos2_exciton.compute_2D_RPAInvDielectricMatrix("MoS2_TB_q_points_test.dat");
+    // hBN_exciton.compute_2D_RPAInvDielectricMatrix("MoS2_TB_q_points_test.dat");
 
-    // mos2_exciton.writeRPAPolarizabilityMatrix("MoS2_RPA_polarizability.dat");
+    // hBN_exciton.writeRPAPolarizabilityMatrix("MoS2_RPA_polarizability.dat");
 
-    // mos2_exciton.writeRPAInverseDielectricMatrix("MoS2_TB_RPA_screening.dat");
+    // hBN_exciton.writeRPAInverseDielectricMatrix("MoS2_TB_RPA_screening.dat");
 
     return 0;
 }
