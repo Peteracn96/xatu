94f58f8ac4de9e1577f139b118f12a42b880120c
Peteracn96 <pacn96@gmail.com>
2025-07-31 18:48:14 +0200

    Improves working with the regularization script

--- main/convergence_regularization.cpp
+++ main/convergence_regularization.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]){
         throw std::invalid_argument("Error: At least one input file is required (system config.)");
     };
     
-    int nbands = 1;
+    int nbands = 2;
     int nrmbands = 0;
     arma::rowvec Q = {0., 0., 0.};
     arma::rowvec parameters = {1., 1., 5.172};
@@ -38,56 +38,57 @@ int main(int argc, char* argv[]){
     double Gc_exciton = 20;
     double Gcutoff = 20.0;
 
-    int ncell = 10;
+    int ncell = 60;
 
-    arma::vec percentage_array = {0.1, 0.2, 0.3, 0.4, 0.45, 0.5, 0.55, 0.6, 0.7, 0.8, 0.9, 1.0};
+    arma::vec percentage_array = {1.1, 1.2, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55};
 
     bool writeEigvals = true;
-    std::string filename = "eigval_convergence_hBN_HSE06_Gc_20_0_with_reg.out";
+    std::string filename = "eigval_convergence_hBN_HSE06_Gc_20_0_with_reg_extra.out";
     FILE* textfile_en = fopen(filename.c_str(), "a");
+
+    cout << "+---------------------------------------------------------------------------+" << endl;
+    cout << "|                                  Parameters                               |" << endl;
+    cout << "+---------------------------------------------------------------------------+" << endl;
+    cout << "N. cells: " << ncell*ncell << endl;
+    cout << "#bands: " << nbands << endl;
+    cout << "System configuration file: " << modelfile << "\n" << endl;
+
+    // xatu::SystemConfiguration config(modelfile);
+
+    std::unique_ptr<xatu::SystemConfiguration> systemConfig;
+    systemConfig.reset(new xatu::CRYSTALConfiguration(modelfile, 100));
+
+    std::cout << "System configuration file parsed." << std::endl;
+    
+    xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(*systemConfig, ncell, nbands, nrmbands, parameters, Q, Gcutoff, Gc_exciton); 
+    // xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(config, ncell, nbands, nrmbands, parameters, Q, Gcutoff, nG); 
+
+    bulkExciton.system->setAU(true); // Comment if input model is not CRYSTAL
+
+    arma::cout << "Orbitals: " << bulkExciton.system->orbitals << arma::endl;
+
+    bulkExciton.setMode("reciprocalspace");
+    bulkExciton.setPotential("keldysh");
+
+    cout << "Valence bands:\n" << bulkExciton.valenceBands << endl;
+    cout << "Conduction bands:\n" << bulkExciton.conductionBands << endl;
+    cout << "Gauge used: " << bulkExciton.gauge << "\n" << endl;
+    
+    cout << "+---------------------------------------------------------------------------+" << endl;
+    cout << "|                                Initialization                             |" << endl;
+    cout << "+---------------------------------------------------------------------------+" << endl;
+
+    bulkExciton.brillouinZoneMesh(ncell);
+    bulkExciton.initializeHamiltonian();
 
     for(int i = 0; i < percentage_array.n_elem; i++){
 
         auto start = high_resolution_clock::now();
 
-        cout << "+---------------------------------------------------------------------------+" << endl;
-        cout << "|                                  Parameters                               |" << endl;
-        cout << "+---------------------------------------------------------------------------+" << endl;
-        cout << "N. cells: " << ncell*ncell << endl;
-        cout << "#bands: " << nbands << endl;
-        cout << "#removed bands: " << nrmbands << endl;
-        cout << "System configuration file: " << modelfile << "\n" << endl;
         cout << "Percentage: " << percentage_array(i) << endl;
 
-        // xatu::SystemConfiguration config(modelfile);
-
-        std::unique_ptr<xatu::SystemConfiguration> systemConfig;
-        systemConfig.reset(new xatu::CRYSTALConfiguration(modelfile, 100));
-
-        std::cout << "System configuration file parsed." << std::endl;
-        
-        xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(*systemConfig, ncell, nbands, nrmbands, parameters, Q, Gcutoff, Gc_exciton); 
-        // xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(config, ncell, nbands, nrmbands, parameters, Q, Gcutoff, nG); 
-
-        bulkExciton.system->setAU(true); // Comment if input model is not CRYSTAL
-
-        arma::cout << "Orbitals: " << bulkExciton.system->orbitals << arma::endl;
-
-        bulkExciton.setMode("reciprocalspace");
-        bulkExciton.setPotential("keldysh");
-
         bulkExciton.setPercentage(percentage_array(i));
-
-        cout << "Valence bands:\n" << bulkExciton.valenceBands << endl;
-        cout << "Conduction bands:\n" << bulkExciton.conductionBands << endl;
-        cout << "Gauge used: " << bulkExciton.gauge << "\n" << endl;
         
-        cout << "+---------------------------------------------------------------------------+" << endl;
-        cout << "|                                Initialization                             |" << endl;
-        cout << "+---------------------------------------------------------------------------+" << endl;
-
-        bulkExciton.brillouinZoneMesh(ncell);
-        bulkExciton.initializeHamiltonian();
         bulkExciton.BShamiltonian();
         auto results = bulkExciton.diagonalize("diag", nstates);
 
