629b0f809593562d21d8ca0f655329432efad350
Peteracn96 <pacn96@gmail.com>
2026-01-26 14:27:12 +0100

    Update convergence with regularization file

--- main/convergence_regularization.cpp
+++ main/convergence_regularization.cpp
@@ -21,16 +21,49 @@ using namespace std::chrono;
 int main(int argc, char* argv[]){
 
     // Parse console stdin
+    int n_args = 5;
 
-    if (argc != 2){
-		throw std::invalid_argument("Error: One input file is expected");
+    if (argc != n_args){
+		throw std::invalid_argument("Error: Two input files are expected");
 	}
-    else if (argc < 2){
-        throw std::invalid_argument("Error: At least one input file is required (system config.)");
+    else if (argc < n_args){
+        throw std::invalid_argument("Error: At least two input file are required (system config, exciton config, screening config and screening data file).");
     };
+
+    std::string modelfile = argv[1];
+    std::string excitonfile = argv[2];
+    std::string screeningfile = argv[3];
+    std::string inv_epsilon_file = argv[4];
+
+    int nstates = 8;
+    int decimals = 6;
+
+    std::unique_ptr<xatu::SystemConfiguration> systemConfig;
+    std::unique_ptr<xatu::ExcitonConfiguration> excitonConfig;
+    std::unique_ptr<xatu::ScreeningConfiguration> screeningConfig;
+
+    if (modelfile.find(".outp") != std::string::npos){
+        systemConfig.reset(new xatu::CRYSTALConfiguration(modelfile, 100));
+    }
+    else if (modelfile.find(".model") != std::string::npos){
+        systemConfig.reset(new xatu::SystemConfiguration(modelfile));
+    } else {
+        throw std::invalid_argument("Error: Unsupported system configuration file format. Use .outp or .model files.");
+    }
+
+    screeningConfig.reset(new xatu::ScreeningConfiguration(screeningfile));
+    excitonConfig.reset(new xatu::ExcitonConfiguration(excitonfile));
+
+
+    xatu::ExcitonTB exciton = xatu::ExcitonTB(*systemConfig, *excitonConfig, *screeningConfig);
+    // xatu::ExcitonTB exciton = xatu::ExcitonTB(*systemConfig, *excitonConfig);
+
+    exciton.setMode(excitonConfig->excitonInfo.mode);
     
-    int nbands = 2;
-    int nrmbands = 0;
+    if (modelfile.find(".outp") != std::string::npos){
+        exciton.system->setAU(true); // if input model is CRYSTAL
+    }
+
     arma::rowvec Q = {0., 0., 0.};
     arma::rowvec parameters = {1., 1., 5.172};
     std::string modelfile = argv[1];    
@@ -38,59 +71,56 @@ int main(int argc, char* argv[]){
     double Gc_exciton = 20;
     double Gcutoff = 20.0;
 
-    int ncell = 60;
+    // int ncell = 60;
 
     arma::vec percentage_array = {1.1, 1.2, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55};
 
     bool writeEigvals = true;
-    std::string filename = "eigval_convergence_hBN_HSE06_Gc_20_0_with_reg_extra.out";
+    std::string filename = "eigval_convergence_hBN_HSE06_Gc_37_0_with_reg.out";
     FILE* textfile_en = fopen(filename.c_str(), "a");
 
     cout << "+---------------------------------------------------------------------------+" << endl;
     cout << "|                                  Parameters                               |" << endl;
     cout << "+---------------------------------------------------------------------------+" << endl;
-    cout << "N. cells: " << ncell*ncell << endl;
-    cout << "#bands: " << nbands << endl;
+    cout << "N. cells: " << exciton.ncell*exciton.ncell << endl;
+
+    // cout << "#bands: " << nbands << endl;
     cout << "System configuration file: " << modelfile << "\n" << endl;
 
     // xatu::SystemConfiguration config(modelfile);
 
-    std::unique_ptr<xatu::SystemConfiguration> systemConfig;
-    systemConfig.reset(new xatu::CRYSTALConfiguration(modelfile, 100));
-
-    std::cout << "System configuration file parsed." << std::endl;
+    // std::cout << "System configuration file parsed." << std::endl;
     
-    xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(*systemConfig, ncell, nbands, nrmbands, parameters, Q, Gcutoff, Gc_exciton); 
-    // xatu::ExcitonTB bulkExciton = xatu::ExcitonTB(config, ncell, nbands, nrmbands, parameters, Q, Gcutoff, nG); 
+    // xatu::ExcitonTB exciton = xatu::ExcitonTB(config, ncell, nbands, nrmbands, parameters, Q, Gcutoff, nG); 
 
-    bulkExciton.system->setAU(true); // Comment if input model is not CRYSTAL
+    exciton.setMode("reciprocalspace");
+    exciton.setPotential("rpa");
 
-    arma::cout << "Orbitals: " << bulkExciton.system->orbitals << arma::endl;
-
-    bulkExciton.setMode("reciprocalspace");
-    bulkExciton.setPotential("keldysh");
-
-    cout << "Valence bands:\n" << bulkExciton.valenceBands << endl;
-    cout << "Conduction bands:\n" << bulkExciton.conductionBands << endl;
-    cout << "Gauge used: " << bulkExciton.gauge << "\n" << endl;
+    cout << "Valence bands:\n" << exciton.valenceBands << endl;
+    cout << "Conduction bands:\n" << exciton.conductionBands << endl;
+    cout << "Gauge used: " << exciton.gauge << "\n" << endl;
     
     cout << "+---------------------------------------------------------------------------+" << endl;
     cout << "|                                Initialization                             |" << endl;
     cout << "+---------------------------------------------------------------------------+" << endl;
 
-    bulkExciton.brillouinZoneMesh(ncell);
-    bulkExciton.initializeHamiltonian();
+    exciton.brillouinZoneMesh(exciton.ncell);
+    exciton.initializeHamiltonian();
 
+    exciton.printInformation();
+
+    exciton.readInverseDielectricMatrix(inv_epsilon_file);
+    
     for(int i = 0; i < percentage_array.n_elem; i++){
 
         auto start = high_resolution_clock::now();
 
         cout << "Percentage: " << percentage_array(i) << endl;
 
-        bulkExciton.setPercentage(percentage_array(i));
+        exciton.setPercentage(percentage_array(i));
         
-        bulkExciton.BShamiltonian();
-        auto results = bulkExciton.diagonalize("diag", nstates);
+        exciton.BShamiltonian();
+        auto results = exciton.diagonalize("diag", nstates);
 
         cout << "+---------------------------------------------------------------------------+" << endl;
         cout << "|                                    Results                                |" << endl;
@@ -106,7 +136,7 @@ int main(int argc, char* argv[]){
 
         if(writeEigvals){
             std::cout << "Writing eigvals to file: " << filename << std::endl;
-            fprintf(textfile_en, "%d\t", ncell);
+            fprintf(textfile_en, "%d\t", exciton.ncell);
             results->writeEigenvalues(textfile_en, 8);
         }
 
