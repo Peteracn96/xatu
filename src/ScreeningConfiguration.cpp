8ce80a311dcc95ceb2b768bd3eeb07b26963198f
Peteracn96 <pacn96@gmail.com>
2026-03-04 17:35:51 +0100

    Remove comment

--- src/ScreeningConfiguration.cpp
+++ src/ScreeningConfiguration.cpp
@@ -82,9 +82,7 @@ void ScreeningConfiguration::parseContent(){
                 std::cout << "Option for spin not recognized, skipping block..." << std::endl;
             }
         } else if(arg == "ncell_aux"){
-            screeningInfo.ncell_aux = parseScalar<int>(content[0]);
-        // } else if(arg == "percentage"){
-        //     screeningInfo.percentage = parseScalar<double>(content[0]);
+            screeningInfo.ncell_aux = parseScalar<int>(content[0]);        
         } else if(arg == "isotropic"){
             std::string isotropic_string = parseWord(content[0]);
             
